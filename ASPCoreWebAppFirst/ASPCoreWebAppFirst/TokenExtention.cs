08300669fcef1fee4e8703f093fc89d822506093
Dmitry Povetkin <lepard_dima@mail.ru>
2018-12-20 15:24:46 +0300

    Создание компонентов middleware

---
+++ ASPCoreWebAppFirst/ASPCoreWebAppFirst/TokenExtention.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreWebAppFirst
+{
+    public static class TokenExtention
+    {
+        public static IApplicationBuilder UseToken(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TokenMiddleware>();
+        }
+    }
+}
