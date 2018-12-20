008c8da1c3e2299ca273fef2117a6bee21eadc0d
Dmitry Povetkin <lepard_dima@mail.ru>
2018-12-20 17:17:06 +0300

    Session1

--- ASPCoreWebAppFirst/ASPCoreWebAppFirst/Startup.cs
+++ ASPCoreWebAppFirst/ASPCoreWebAppFirst/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ASPCoreWebAppFirst
 {
@@ -17,31 +18,27 @@ namespace ASPCoreWebAppFirst
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-           // services.AddMvc();
+            // services.AddMvc();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            app.UseSession();
 
-            app.UseOwin(pipeline =>
+            app.Run(async (context) =>
             {
-                pipeline(next => SendResponseAsync);
+                if (context.Session.Keys.Contains("name"))
+                    await context.Response.WriteAsync($"Hello {context.Session.GetString("name")}!");
+                else
+                {
+                    context.Session.SetString("name", "Dima");
+                    await context.Response.WriteAsync("Hello world");
+                }
             });
-
         }
 
-        private Task SendResponseAsync(IDictionary<string, object> environment)
-        {
-            // получаем заголовки запроса
-            var requestHeaders = (IDictionary<string, string[]>)environment["owin.RequestHeaders"];
-            // получаем данные по User-Agent
-            string responseText = requestHeaders["User-Agent"][0];
-            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
-
-            var responseStream = (Stream)environment["owin.ResponseBody"];
-
-            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
-        }
     }
 }
