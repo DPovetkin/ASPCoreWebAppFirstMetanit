8de408182015c1aeb2ce961fa350edadd64fb5e0
Dmitry Povetkin <lepard_dima@mail.ru>
2018-12-21 12:32:32 +0300

    Использование фабрики сервисов

--- ASPCoreWebAppFirst/ServicesLifeCycle/Startup.cs
+++ ASPCoreWebAppFirst/ServicesLifeCycle/Startup.cs
@@ -8,8 +8,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using ServicesLifeCycle.Services;
 
-// AddSingleton
-// Для создания singleton-объектов необязательно полагаться на механизм DI.Мы его можем сами создать и передать в нужный метод:
+// Использование фабрики сервисов
+// Ряд перегруженных версий у всех трех методов в качестве параметра могут принимать 
+// фабрику сервисов - Func<IServiceProvider, object> implementationFactory, которая управляет созданием объектов сервисов.
+// Фабрика позволяет нам применить более сложную логику по созданию сервиса, например, добавить условия создания:
+
+
 
 namespace ServicesLifeCycle
 {
@@ -18,11 +22,26 @@ namespace ServicesLifeCycle
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {
 
-            RandomCounter randomCounter = new RandomCounter();
-            services.AddSingleton<ICounter>(randomCounter);
-            services.AddSingleton<CounterService>(new CounterService(randomCounter));
+            services.AddTransient<RandomCounter>();
+            services.AddTransient<ICounter>(provider =>
+           {
+               var counter = provider.GetService<RandomCounter>();
+               return counter;
+           });
+
+
+            // ли к примеру у нас есть зависимость IMessageSender и есть две ее реализации - EmailMessageSender и SmsMessageSender:
+            //services.AddTransient<IMessageSender>(provider =>
+            //{
+            //    if (DateTime.Now.Hour >= 12)
+            //        return new EmailMessageSender();
+            //    else
+            //        return new SMSMessageSender();
+            //});
+
+            services.AddTransient<CounterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
