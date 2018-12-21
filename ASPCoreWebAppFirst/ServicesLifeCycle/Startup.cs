44d20e51204bbb6ffa14e8ad8a7b4d0e6f4b1f73
Dmitry Povetkin <lepard_dima@mail.ru>
2018-12-21 12:22:17 +0300

    Жизненный цикл: Singleton 1

--- ASPCoreWebAppFirst/ServicesLifeCycle/Startup.cs
+++ ASPCoreWebAppFirst/ServicesLifeCycle/Startup.cs
@@ -8,13 +8,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using ServicesLifeCycle.Services;
 
-// AddScoped
-// Метод AddScoped создает один экземпляр объекта для всего запроса.
-// Он имеет те же перегруженные версии, что и AddTransient.
-// Для его применения изменим метод ConfigureServices() в классе Startup
+// AddSingleton
+// AddSingleton создает один объект для всех последующих запросов, при этом объект создается только тогда, когда он непосредственно необходим.
+// Этот метод имеет все те же перегруженые версии, что и AddTransient и AddScoped.
 
-// Теперь в рамках одного и того же запроса и CounterMiddleware и сервис CounterService будут использовать один и тот же объект RandomCounter.
-// При следующем запросе к приложению будет генерироваться новый объект RandomCounter
+// Для применения AddSingleton изменим метод ConfigureServices():
 
 namespace ServicesLifeCycle
 {
@@ -24,8 +22,8 @@ namespace ServicesLifeCycle
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ICounter, RandomCounter>();
-            services.AddScoped<CounterService>();
+            services.AddSingleton<ICounter, RandomCounter>();
+            services.AddSingleton<CounterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
